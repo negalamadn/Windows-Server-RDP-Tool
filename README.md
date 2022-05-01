f06f4c4d3a337a310bcd2851c4399147f19e5aa4
Michael Shirazi <shirazi.michael@yahoo.com>
2022-05-01 12:38:53 -0700

    Windows server RDP tool

---
+++ README.md
@@ -0,0 +1,3 @@
+On my last team, I would have to RDP into countless servers each day. I made this tool so that I could avoid having to search IP addresses or remember the server's hostname. 
+
+The only required input is the server's location number. After you input the location number, if the hostname can be resolved to an IP, the script then RDP's you into the server. I wanted to automate inputing credentials as well but was advised against doing so for compliance reasons.
---
+++ WindowsServerRDP.ps1
@@ -0,0 +1,43 @@
+﻿Write-Host  "
+
+    / ======= \
+   / __________\
+  | ___________ |
+  | | -       | |
+  | |         | |
+  | |_________| |________________________
+  \=____________/   Fuel Server RDP Tool )
+  / ''''''''''' \                       /
+ / ::::::::::::: \                  
+(_________________) 
+
+"
+                                                               
+
+
+while($true){
+    $location = Read-Host "Enter The Store Number"
+    $USDNS = "USFUEL" + $location.PadLeft(5,'0') + "S1"
+    $CADNS = "CAFUEL" + $location.PadLeft(5,'0') + "S1"
+    $ErrorActionPreference = "silentlycontinue"
+    $IP1 = (Resolve-DnsName $USDNS).address
+    $IP2 = (Resolve-DnsName $CADNS).address 
+    $ErrorActionPreference = "continue"
+    
+        if($IP1)
+        {
+        mstsc.exe /v:$IP1 
+        }
+    
+        elseif($IP2)
+        {
+        mstsc.exe /v:$IP2 
+        } 
+        else{
+        Write-Host -ForegroundColor Red "Invalid Location Number"
+        }
+
+
+      }
+
+
